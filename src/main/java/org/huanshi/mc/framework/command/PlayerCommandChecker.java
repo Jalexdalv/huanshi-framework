7b0ceb901977bcdb7199c5dee62b366c065ed62f
Jalexdalv <gaoyihang@aliyun.com>
2023-02-22 18:11:15 +0800

    提交更新

--- src/main/java/org/huanshi/mc/framework/command/PlayerCommandChecker.java
+++ src/main/java/org/huanshi/mc/framework/command/PlayerCommandChecker.java
@@ -10,22 +10,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class PlayerCommandChecker extends AbstractListener {
-    private Set<String> playerCommandNameSet = new HashSet<>();
-    private Map<String, AbstractPlayerCommand> commonPlayerCommandMap = new HashMap<>(), opPlayerCommandMap = new HashMap<>();
+    private final List<String> playerCommandNameList = new LinkedList<>();
+    private final Map<String, AbstractPlayerCommand> commonPlayerCommandMap = new HashMap<>(), opPlayerCommandMap = new HashMap<>();
 
     @EventHandler
     public void onComponentScanComplete(@NotNull ComponentScanCompleteEvent componentScanCompleteEvent) {
         for (Component component : componentScanCompleteEvent.getComponents()) {
             if (component instanceof AbstractPlayerCommand playerCommand) {
                 String name = playerCommand.getName();
-                playerCommandNameSet.add(name);
+                playerCommandNameList.add(name);
                 if (playerCommand.isOp()) {
                     opPlayerCommandMap.put(name, playerCommand);
                 } else {
@@ -33,28 +31,24 @@ public class PlayerCommandChecker extends AbstractListener {
                 }
             }
         }
-        playerCommandNameSet = Collections.unmodifiableSet(playerCommandNameSet);
-        commonPlayerCommandMap = Collections.unmodifiableMap(commonPlayerCommandMap);
-        opPlayerCommandMap = Collections.unmodifiableMap(opPlayerCommandMap);
     }
 
     public boolean check(@NotNull Player player, @NotNull String name) {
-        if (player.isOp()) {
-            return true;
-        }
-        return commonPlayerCommandMap.containsKey(name);
+        return commonPlayerCommandMap.containsKey(name) || (player.isOp() && opPlayerCommandMap.containsKey(name));
     }
 
     public @NotNull Collection<String> getPlayerCommandNames(@NotNull Player player) {
+        List<String> playerCommandNameList;
         if (player.isOp()) {
-            return playerCommandNameSet;
-        }
-        List<String> playerCommandNameList = new LinkedList<>();
-        for (AbstractPlayerCommand playerCommand : commonPlayerCommandMap.values()) {
-            if (playerCommand.hasPermission(player)) {
-                playerCommandNameList.add(playerCommand.getName());
+            playerCommandNameList = this.playerCommandNameList;
+        } else {
+            playerCommandNameList = new LinkedList<>();
+            for (AbstractPlayerCommand playerCommand : commonPlayerCommandMap.values()) {
+                if (playerCommand.hasPermission(player)) {
+                    playerCommandNameList.add(playerCommand.getName());
+                }
             }
         }
-        return playerCommandNameList;
+        return Collections.unmodifiableList(playerCommandNameList);
     }
 }
